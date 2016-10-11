02f670565b0c81f1ec7480b15d4c05108f87a16a
Michael Sevestre <michael.sevestre@gmail.com>
2016-10-11 12:21:46 -0400

    Create first assembly

---
+++ dev/node3.1.Tests/FileHelperSpecs.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace node3_1
+{
+   [TestFixture]
+   public class FileHelperSpecs
+   {
+      [Test]
+      public void TestOne()
+      {
+         Assert.IsTrue(true);
+      }
+   }
+}
