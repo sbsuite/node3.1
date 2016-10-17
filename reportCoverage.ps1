c68dbf6a8a37ac0acd9947c99796084950e77ef7
Robert <rob@robertmcintosh.ca>
2016-10-17 17:41:57 -0400

    move script to ps1 file

---
+++ reportCoverage.ps1
@@ -0,0 +1,2 @@
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:nunit3-console.exe -targetargs:"dev\node3.1.Tests\node3.1.Tests.csproj /config:Release"
+packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i TestResult.xml --repoToken 6OekI2g4GgswsVomBpAM5ZqglIGi6Glm8
