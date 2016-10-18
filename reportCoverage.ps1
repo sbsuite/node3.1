ed14af677323d8f0352b27ea89916a6f971b7177
Robert <rob@robertmcintosh.ca>
2016-10-18 07:22:15 -0400

    encrypt COVERALLS_REPO_TOKEN

--- reportCoverage.ps1
+++ reportCoverage.ps1
@@ -1,2 +1,2 @@
 packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:nunit3-console.exe -targetargs:"dev\node3.1.Tests\node3.1.Tests.csproj /config:Release" -filter:+[*]*-[*.Tests]*
-packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i TestResult.xml --repoToken 6OekI2g4GgswsVomBpAM5ZqglIGi6Glm8
+packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i TestResult.xml --repoToken COVERALLS_REPO_TOKEN
