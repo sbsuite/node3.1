e415a7792c808a708cbc36cd56bfed32896189e9
Robert <rob@robertmcintosh.ca>
2016-10-18 09:46:09 -0400

    actually analyze output of opencover

--- reportCoverage.ps1
+++ reportCoverage.ps1
@@ -1,8 +1,8 @@
-packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:nunit3-console.exe -targetargs:"dev\node3.1.Tests\node3.1.Tests.csproj /config:Release"
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:nunit3-console.exe -targetargs:"dev\node3.1.Tests\node3.1.Tests.csproj /config:Release" -filter:+[*]*-[*.Tests]* -output:OpenCover.xml
 $coveralls = (Resolve-Path "packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
 $env:APPVEYOR_BUILD_NUMBER
-& $coveralls --opencover -i TestResult.xml --repoToken $env:COVERALLS_REPO_TOKEN --useRelativePaths --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_BUILD_NUMBER --serviceName appveyor
+& $coveralls --opencover -i OpenCover.xml --repoToken $env:COVERALLS_REPO_TOKEN --useRelativePaths --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_BUILD_NUMBER --serviceName appveyor
 
 $reportGenerator = (Resolve-Path "packages/ReportGenerator.*/tools/ReportGenerator.exe").ToString()
-& $reportGenerator "-reports:TestResult.xml" "-targetdir:.\coverage"
+& $reportGenerator "-reports:OpenCover.xml" "-targetdir:.\coverage"
 copy TestResult.xml coverage\
