10b4dfce07efe1173abd1f88df326a82ce57c2e0
Robert <rob@robertmcintosh.ca>
2016-10-18 07:31:44 -0400

    different coveralls.net script

--- reportCoverage.ps1
+++ reportCoverage.ps1
@@ -1,2 +1,7 @@
 packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:nunit3-console.exe -targetargs:"dev\node3.1.Tests\node3.1.Tests.csproj /config:Release" -filter:+[*]*-[*.Tests]*
-packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i TestResult.xml
+
+$coveralls = (Resolve-Path "packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
+    
+$env:APPVEYOR_BUILD_NUMBER
+    
+& $coveralls --opencover -i TestResults.xml --repoToken $env:COVERALLS_REPO_TOKEN --useRelativePaths --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_BUILD_NUMBER --serviceName appveyor
