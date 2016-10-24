5b465d8f207311f8da453ff331e8d1c079e922be
Robert <rob@robertmcintosh.ca>
2016-10-24 16:38:24 -0400

    adding installer to modify copy to output

--- dev/NativeLibrary/install.ps1
+++ dev/NativeLibrary/install.ps1
@@ -1,1 +1,5 @@
-Copy-Item ./**/*.dll ./NativeLibs
+param($installPath, $toolsPath, $package, $project)
+$file = $project.ProjectItems.Item("NativeLibrary.dll")
+ 
+$copyToOutput = $file.Properties.Item("CopyToOutputDirectory")
+$copyToOutput.Value = 2
