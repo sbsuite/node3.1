986814337d092a90e08fd145d669284f071e443f
rwmcintosh <rob@robertmcintosh.ca>
2016-10-26 14:21:16 -0400

    copy dlls to native folder in project directory

--- dev/NativeLibrary/install.ps1
+++ dev/NativeLibrary/install.ps1
@@ -3,3 +3,9 @@ param($installPath, $toolsPath, $package, $project)
 $installPath
 
 $toolsPath
+
+$projectFullName = $project.FullName
+$fileInfo = new-object -typename System.IO.FileInfo -ArgumentList $projectFullName
+$projectDirectory = $fileInfo.DirectoryName
+
+copy $installPath/native/*.dll $projectDirectory/native/
