137dff586d6b4158224f7f065cf777bb94a53043
rwmcintosh <rob@robertmcintosh.ca>
2016-10-26 14:24:34 -0400

    write-host to debug

--- dev/NativeLibrary/install.ps1
+++ dev/NativeLibrary/install.ps1
@@ -8,4 +8,6 @@ $projectFullName = $project.FullName
 $fileInfo = new-object -typename System.IO.FileInfo -ArgumentList $projectFullName
 $projectDirectory = $fileInfo.DirectoryName
 
+Write-Host "copy ./$installPath/native/*.dll $projectDirectory/native/" -ForegroundColor Green
+
 copy $installPath/native/*.dll $projectDirectory/native/
