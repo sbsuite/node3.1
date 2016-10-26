e937781a1e354b36007b8fe4f218a2e915e0b6d4
rwmcintosh <rob@robertmcintosh.ca>
2016-10-26 14:06:54 -0400

    change installation method

--- dev/NativeLibrary/install.ps1
+++ dev/NativeLibrary/install.ps1
@@ -1,5 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
-$file = $project.ProjectItems.Item("NativeLibrary.dll")
- 
-$copyToOutput = $file.Properties.Item("CopyToOutputDirectory")
-$copyToOutput.Value = 2
+
+$installPath
+
+$toolsPath
